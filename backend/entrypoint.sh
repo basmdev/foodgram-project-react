8ea9f479cac705f577d9f8e70ff54d8061e1e638
Мохьмад Басханов <basm@upr.local>
2023-07-27 14:25:59 +0300

    Правки

--- backend/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python -m gunicorn --bind 0.0.0.0:8000 foodgram.wsgi
-python backend/manage.py migrate
-python backend/manage.py collectstatic
-cp -r /app/collected_static/. /static/static/
